2916098f7dda117796f058a49952156e82dca23d
Dennis So <so.dennis@hotmail.com>
2020-11-18 22:04:45 +0000

    Bumped to 0.0.23

--- ipymonaco/_version.py
+++ ipymonaco/_version.py
@@ -1,5 +1,5 @@
 # Module version
-version_info = (0, 0, 22, 'alpha', 0)
+version_info = (0, 0, 23, 'alpha', 0)
 
 # Module version stage suffix map
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
